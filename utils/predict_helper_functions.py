de0be75ef64b10fe35ac904462f195f14ba34832
Florian Fabry <florian.fabry@posteo.de>
2024-04-25 18:56:38 +0200

    Adjust "tribuene" masks and change to 720p.

--- utils/predict_helper_functions.py
+++ utils/predict_helper_functions.py
@@ -14,8 +14,7 @@ from utils.database_helper_functions import download_model
 # ------------------------------------------------------------------------------
 # Ancillary definitions
 # ------------------------------------------------------------------------------
-max_width = 1920
-max_height = 1080
+max_width, max_height = 1280, 720
 device = torch.device("cpu")
 
 # ------------------------------------------------------------------------------
