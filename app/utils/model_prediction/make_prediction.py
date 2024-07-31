ef4e484217c18e8e97081898c18c0457646168a4
Florian Fabry <florian.fabry@posteo.de>
2024-07-31 02:11:57 +0200

    change to 900p.

--- app/utils/model_prediction/make_prediction.py
+++ app/utils/model_prediction/make_prediction.py
@@ -12,7 +12,7 @@ from app.utils.database_helper_functions import download_model
 # ------------------------------------------------------------------------------
 # Helper definitions and functions
 # ------------------------------------------------------------------------------
-fixed_width, fixed_height = 1920, 1080
+fixed_width, fixed_height = 1600, 900
 device = torch.device("cpu")
 
 # ------------------------------------------------------------------------------
