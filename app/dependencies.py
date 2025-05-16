dab9e053609e700097719301a8bd4ab0c3431373
tensorwitz <niklas.witzel@tensora.co>
2025-05-16 21:07:29 +0200

    Introduce borg pattern for dependencies

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,5 @@
 import os
-from typing import Dict, Annotated, Optional
-from functools import lru_cache
+from typing import Dict, Annotated
 
 from fastapi import Depends
 from azure.cosmos import ContainerProxy
@@ -15,61 +14,109 @@ from app.utils.model_prediction.make_prediction import initialize_model
 from app.utils.model_prediction.dm_count import DMCount
 
 
-# Use lru_cache to implement the singleton pattern
-@lru_cache(maxsize=1)
+# Base Borg class - all derived classes will share state within their class
+class Borg:
+    _shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self._shared_state
+
+
+class ProjectRepositoryBorg(Borg):
+    def __init__(self) -> None:
+        Borg.__init__(self)
+        if not hasattr(self, "project_repository"):
+            print("Initializing ProjectRepository")
+            self.project_repository = ProjectRepository()
+            self.project_repository.initialize()
+
+
+class ProjectServiceBorg(Borg):
+    def __init__(self, repository: ProjectRepository):
+        Borg.__init__(self)
+        if not hasattr(self, "project_service"):
+            print("Initializing ProjectService")
+            self.project_service = ProjectService(repository)
+
+
+class CameraServiceBorg(Borg):
+    def __init__(self, project_service: ProjectService):
+        Borg.__init__(self)
+        if not hasattr(self, "camera_service"):
+            print("Initializing CameraService")
+            self.camera_service = CameraService(project_service)
+
+
+class ModelManagerBorg(Borg):
+    def __init__(self):
+        Borg.__init__(self)
+        if not hasattr(self, "models"):
+            print("Initializing ML models")
+            self.models = {
+                CountingModel.STANDARD: initialize_model(
+                    os.environ["STANDARD_MODEL_NAME"]
+                ),
+                CountingModel.LIGHTSHOW: initialize_model(
+                    os.environ["LIGHTSHOW_MODEL_NAME"]
+                ),
+            }
+
+
+class CosmosClientBorg(Borg):
+    def __init__(self):
+        Borg.__init__(self)
+        if not hasattr(self, "client"):
+            print("Initializing CosmosDB client")
+            self.client = create_cosmos_db_client("predictions")
+
+
+class PredictionServiceBorg(Borg):
+    def __init__(
+        self,
+        camera_service: CameraService,
+        models: Dict[CountingModel, DMCount],
+        cosmosdb_client: ContainerProxy,
+    ):
+        Borg.__init__(self)
+        if not hasattr(self, "prediction_service"):
+            print("Initializing PredictionService")
+            self.prediction_service = PredictionService(
+                self,
+                camera_service=camera_service,
+                models=models,
+                cosmosdb_client=cosmosdb_client,
+            )
+
+
 def get_project_repository() -> ProjectRepository:
-    """Singleton for ProjectRepository."""
-    print("Initializing ProjectRepository")
-    repository = ProjectRepository()
-    repository.initialize()
-    return repository
+    return ProjectRepositoryBorg().project_repository
 
 
-@lru_cache(maxsize=1)
 def get_project_service(
     repository: Annotated[ProjectRepository, Depends(get_project_repository)],
 ) -> ProjectService:
-    """Singleton for ProjectService."""
-    print("Initializing ProjectService")
-    return ProjectService(repository)
+    return ProjectServiceBorg(repository).project_service
 
 
-@lru_cache(maxsize=1)
 def get_camera_service(
     project_service: Annotated[ProjectService, Depends(get_project_service)],
 ) -> CameraService:
-    """Singleton for CameraService."""
-    print("Initializing CameraService")
-    return CameraService(project_service)
+    return CameraServiceBorg(project_service).camera_service
 
 
-@lru_cache(maxsize=1)
 def get_models() -> Dict[CountingModel, DMCount]:
-    """Singleton for ML models."""
-    print("Initializing ML models")
-    return {
-        CountingModel.STANDARD: initialize_model(os.environ["STANDARD_MODEL_NAME"]),
-        CountingModel.LIGHTSHOW: initialize_model(os.environ["LIGHTSHOW_MODEL_NAME"]),
-    }
+    return ModelManagerBorg().models
 
 
-@lru_cache(maxsize=1)
 def get_cosmosdb_client() -> ContainerProxy:
-    """Singleton for CosmosDB client."""
-    print("Initializing CosmosDB client")
-    return create_cosmos_db_client("predictions")
+    return CosmosClientBorg().client
 
 
-@lru_cache(maxsize=1)
 def get_prediction_service(
     camera_service: Annotated[CameraService, Depends(get_camera_service)],
     models: Annotated[Dict[CountingModel, DMCount], Depends(get_models)],
     cosmosdb_client: Annotated[ContainerProxy, Depends(get_cosmosdb_client)],
 ) -> PredictionService:
-    """Singleton for PredictionService."""
-    print("Initializing PredictionService")
-    return PredictionService(
-        camera_service=camera_service,
-        models=models,
-        cosmosdb_client=cosmosdb_client,
-    )
+    return PredictionServiceBorg(
+        camera_service, models, cosmosdb_client
+    ).prediction_service
